f08ce9c760bc92a6f8efa4701509bcc9550ec725
Pankaj Malhotra(:bitgeeky) <devpankaj1994@gmail.com>
2015-05-25 20:19:11 +0530

    initial

---
+++ new_test.js
@@ -0,0 +1,11 @@
+;(function(){
+console.log('start of the javascript 1');
+if(window.top.location.host != window.location.host)
+        throw new Error("Not the main page");
+if(parent.document.URL!=document.location.href)
+        throw new Error("Not the main page")
+console.log('end of the javascrit 2');
+//(function(e){e.setAttribute("src","http://localhost:8081/target/target-script-min.js#anonymous");document.getElementsByTagName("body")[0].appendChild(e);})(document.createElement("script"));void(0);
+(function(e){e.setAttribute("src","https://lit-sands-8462.herokuapp.com/target/target-script-min.js#anonymous");document.getElementsByTagName("body")[0].appendChild(e);})(document.createElement("script"));void(0);
+console.log('script injected');
+})();
